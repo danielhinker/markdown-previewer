2c1f2f46b80b65deb5ee43767ca29d9d03b4be4b
Daniel Hinker <danhinker@gmail.com>
2019-08-29 20:31:51 -0400

    Updated files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
+import marked from 'marked';
 import './App.css';
 
-function App() {
+const renderer = new marked.Renderer();
+// const marked = require('marked');
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  breaks: true,
+
+});
+const start =
+  `
+  # Header
+  ## Sub Header
+  [link](https://google.com)
+  \` Inline Code \`
+  \`\`\`
+  Code Block
+  \`\`\`
+  ![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")
+  **bold**
+  1. OL
+  * UL
+  > Block Quote
+  `
+
+
+const Preview = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div id="preview" dangerouslySetInnerHTML={{__html: marked(props.markup, { renderer: renderer })}}/>
   );
 }
 
+const Editor = (props) => {
+  return (
+    <textarea id="editor" value={props.markup} onChange={props.onChange} type="text" />
+  );
+}
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      markup: start
+    };
+    this.handleChange = this.handleChange.bind(this)
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      
+        markup: e.target.value
+      
+    });
+  }
+
+    render() {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <Editor onChange={this.handleChange} markup={this.state.markup} />
+            
+            <Preview markup={this.state.markup} />
+            
+          </header>
+        </div>
+      );
+  }
+}
 export default App;
